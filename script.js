9bdee6fd06a8c07e79c9b045bcaa0fd7f5ce425f
pranavikadatla <142671406+pranavikadatla@users.noreply.github.com>
2023-10-12 15:44:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
+	for(let i=0; i<str.length;i++){
+		if(str.charAt(i)!=str.charAt(i+1)){
+			return str.charAt(i);
+			}
+		else{
+			i++;
+		}
+		}
+	return null;
+	}
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
