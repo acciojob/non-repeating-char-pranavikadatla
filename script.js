91fa7b633542192ed4aa8f1aa331dde650d8ddfc
pranavikadatla <142671406+pranavikadatla@users.noreply.github.com>
2023-12-22 14:17:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	for(let i=0; i<str.length;i++){
-		if(str.charAt(i)!=str.charAt(i+1)){
-			return str.charAt(i);
-			}
+		 let currentChar = str.charAt(i);
+        // Check if the current character is not repeated in the entire string
+        if (str.indexOf(currentChar) === str.lastIndexOf(currentChar)) {
+            return currentChar;
+		
 		else{
 			i++;
 		}
